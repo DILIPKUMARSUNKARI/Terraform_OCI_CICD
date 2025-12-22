9e95060ac8e4ee7b8686dc731cc9e2f1d05b3516
DILIP <104927524+DILIPKUMARSUNKARI@users.noreply.github.com>
2025-12-22 19:49:38 +0530

    Update Terraform backend address configuration

--- OracleCloudInfrastructure/Terraform/terraform.tf
+++ OracleCloudInfrastructure/Terraform/terraform.tf
@@ -11,8 +11,8 @@ terraform {
 
 terraform {
   backend "http" {
-    #address       = "https://objectstorage.ap-hyderabad-1.oraclecloud.com/p/8163yWk19sbpcLEshS1iIoGxqMGcAhC4BIekXs--ISsGuXbbxFAmXYPyl5fHWIyj/n/axojviqbbs15/b/my_cicd/o/terraform.tfstate"
-     address= "https://axojviqbbs15.objectstorage.ap-hyderabad-1.oci.customer-oci.com/n/axojviqbbs15/b/my_cicd/o/terraform.tfstate"
+    address  = "https://objectstorage.ap-hyderabad-1.oraclecloud.com/p/8163yWk19sbpcLEshS1iIoGxqMGcAhC4BIekXs--ISsGuXbbxFAmXYPyl5fHWIyj/n/axojviqbbs15/b/my_cicd/o/terraform.tfstate"
+     #address= "https://axojviqbbs15.objectstorage.ap-hyderabad-1.oci.customer-oci.com/n/axojviqbbs15/b/my_cicd/o/terraform.tfstate"
      update_method = "PUT"
 
   }
